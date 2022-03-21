0aab03d1c09e46e4d50af3eacc7247996ab72b10
James (DPC) <james.ellis@dpc.nsw.gov.au>
2022-03-21 13:52:04 +1100

    do not check status if asset is a folder

--- src/Extensions/AssetExtension.php
+++ src/Extensions/AssetExtension.php
@@ -3,6 +3,7 @@
 namespace NSWDPC\Authentication;
 
 use SilverStripe\Assets\File;
+use SilverStripe\Assets\Folder;
 use SilverStripe\Core\Extension;
 
 /**
@@ -12,10 +13,14 @@ use SilverStripe\Core\Extension;
 class AssetExtension extends Extension {
 
     /**
-     * Pending members cannot view files
-     * @param Member $member
+     * Handle pending members accessing files
+     * @param Member|null $member
+     * @return mixed
      */
     public function canView($member) {
+        if($this->owner instanceof Folder) {
+            return null;
+        }
         if($member && $member->getIsPending()) {
             return false;
         }
